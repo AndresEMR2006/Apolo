b48ce10c3866d6256d1d3fa2db43f6ff5572c0e2
Andres <andresemr2006@outlook.es>
2026-09-15 00:33:38 -0500

    feat: mostrar tracks en el frontend

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -13,4 +13,29 @@ async function obtenerEstado() {
   }
 }
 
+async function obtenerCanciones() {
+  try {
+    const response = await fetch("http://localhost:8080/api/tracks");
+
+    if (!response.ok) {
+      throw new Error(`Error en la solicitud: ${response.status}`);
+    }
+
+    const data = await response.json();
+
+    console.log(data);
+    console.log(data[0]);
+
+    const listaCanciones = document.getElementById("lista-canciones");
+    data.forEach((cancion) => {
+      const elemento = document.createElement("li");
+      elemento.textContent = cancion.name + "-" + cancion.artists;
+      listaCanciones.appendChild(elemento);
+    });
+  } catch (error) {
+    console.error("Error al obtener el mensaje:", error);
+  }
+}
+
 obtenerEstado();
+obtenerCanciones();
